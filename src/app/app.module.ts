c79a3cf6d387419b7b1fd7e86fcd8f2345012f1c
dewipurwati17 <dewipurwati880@gmail.com>
2017-06-12 09:12:04 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item-page/add-item-page';
+import { ItemDetailPage } from '../pages/item-detail-page/item-detail-page';
+ 
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  providers: []
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item-page/add-item-page'
+import { ItemDetailPage } from '../item-detail-page/item-detail-page'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ 
+  public items = [];
+ 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController ) {
+    
+  }
+ 
+  ionViewDidLoad(){
+  
+  }
+ 
+  addItem(){
+ 
+    let addModal = this.modalCtrl.create(AddItemPage);
+    // call back when modal dismissed
+    addModal.onDidDismiss((item) => {
+      if(item){
+        this.saveItem(item);
+      }
+    });
+    addModal.present();
+    
+  }
+ 
+  viewItem(item){
+  this.navCtrl.push(ItemDetailPage, {
+    item: item
+  });
+}
+
+  saveItem(item){
+    this.items.push(item);
+  }
+ 
+}
